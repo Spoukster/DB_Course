6cc77dda3c200acbc5dbb96a42fd404c31d52314
Spoukster <spoukster@gmail.com>
2019-04-28 00:50:23 +0300

    Заполнена таблица products в shop.sql

--- shop.sql
+++ shop.sql
@@ -76,6 +76,18 @@ CREATE TABLE products (
 -- CREATE INDEX index_of_catalog_id ON products (catalog_id);
 -- DROP INDEX index_of_catalog_id ON products;
 
+INSERT INTO products
+	(name, description, price, catalog_id)
+VALUES
+	('Intel Core i3-8100', 'Процессор для настольных ПК, основанных на платформе Intel.', 7890.00, 1),
+    ('Inte Core i5-7400', 'Процессор для настольных ПК, основанных на платформе Intel.', 12700.00, 1),
+    ('AMD FX-8320E', 'Процессор для настольных ПК, основанных на платформе AMD.', 4780.00, 1),
+    ('AMD FX-8320', 'Процессор для настольных ПК, основанных на платформе AMD.', 7120.00, 1),
+    ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+    ('Gigabyte H310 S2H', 'Материнская плата Gigabyte H310 S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+    ('MSI 8250M GAMING PRO', 'Материнская плата MSI 8250M GAMING PRO, 8250, Socket 1151, DDR4, mATX', 5060.00, 2);
+SELECT * FROM products;
+
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
 	id SERIAL PRIMARY KEY,
